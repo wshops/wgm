daf260a84c1f54297cfaf3678d1de123f84715ab
Dr-dddd <1620210600@qq.com>
2023-01-12 16:57:39 +0800

    :white_check_mark:  添加Benchmark测试

--- updater_test.go
+++ updater_test.go
@@ -13,7 +13,7 @@ func TestGetUpdater(t *testing.T) {
 
 }
 
-func TestUpdaterFind(t *testing.T) {
+func TestUpdater_Find(t *testing.T) {
 	DBdoc := NewDoc("Alice", 12)
 	SetupDefaultConnection()
 	objectID := InsertDoc(DBdoc)
@@ -47,4 +47,40 @@ func TestUpdater_Update(t *testing.T) {
 	wgm.FindOne(DBdoc, nil)
 	require.Equal(t, DBdoc.Age, 99)
 	require.Equal(t, DBdoc.Name, "dada")
+}
+
+func BenchmarkUpdater_Find(b *testing.B) {
+	DBdoc := NewDoc("Alice", 12)
+	SetupDefaultConnection()
+	objectID := InsertDoc(DBdoc)
+	defer DelDoc(DBdoc)
+
+	doc := &Doc{}
+	doc.Id = objectID
+	u, hasResult := wgm.Updater(doc).Find()
+
+	require.True(b, hasResult)
+	require.NotNil(b, u)
+	require.Equal(b, doc.Age, DBdoc.Age)
+	require.Equal(b, doc.Name, DBdoc.Name)
+}
+
+func BenchmarkUpdater_Update(b *testing.B) {
+	DBdoc := NewDoc("Alice", 12)
+	SetupDefaultConnection()
+	objectID := InsertDoc(DBdoc)
+	defer DelDoc(DBdoc)
+
+	doc := &Doc{}
+	doc.Id = objectID
+	u, _ := wgm.Updater(doc).Find()
+	doc.Name = "dada"
+	doc.Age = 99
+	err := u.Update()
+	require.Nil(b, err)
+
+	DBdoc = &Doc{}
+	wgm.FindOne(DBdoc, nil)
+	require.Equal(b, DBdoc.Age, 99)
+	require.Equal(b, DBdoc.Name, "dada")
 }
